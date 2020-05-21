aca04dc156d35426c45e5d04beb37a15876772de
David Valenzuela <davidevc.32@gmail.com>
2020-05-21 18:45:00 -0400

    Uso y llamado de una funcion

--- tarea.adb
+++ tarea.adb
@@ -1,11 +1,16 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
-
 procedure Tarea is
+
+function Increment (I : Integer) return Integer;
+
+function Increment (I : Integer) return Integer is
+begin
+    return I + 1;
+end Increment;
 
 
 begin
-
-null;
+Put(Integer'Image(Increment(5)));
 end Tarea;
