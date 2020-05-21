ce5a5b85cb342753f9992dde64b37c050f4be800
David Valenzuela <davidevc.32@gmail.com>
2020-05-21 19:06:13 -0400

    Funcion y llamada de un procedimiento

--- tarea.adb
+++ tarea.adb
@@ -4,34 +4,21 @@ with Increment_By;
 
 procedure Tarea is
 
-A, B, C, D : Integer;
+   A, B, C, D : Integer;
+
+   procedure Display_Result is
+   begin
+      Put_Line ("Increment of " & Integer'Image (A)
+                & " with "      & Integer'Image (B)
+                & " multiple "  & Integer'Image (D)
+                & " is "        & Integer'Image (C));
+   end Display_Result;
+
+
 begin
-   C := Increment_By;
-
-   Put_Line ("Using defaults for Increment_By is "
-             & Integer'Image (C));
-
    A := 10;
    B := 3;
    D := 10;
    C := Increment_By (A, B, D);
-   --                 ^ Regular parameter passing
-
-   Put_Line ("Increment of " & Integer'Image (A)
-             & " with "      & Integer'Image (B)
-             & " multiple "  & Integer'Image (D)
-             & " is "        & Integer'Image (C));
-
-   A := 20;
-   B := 5;
-   D := 10;
-   C := Increment_By (I    => A,
-                      Incr => B,
-                      multp => D);
-    --                ^ Named parameter passing
-
-   Put_Line ("Increment of " & Integer'Image (A)
-             & " with "      & Integer'Image (B)
-             & " multiple "  & Integer'Image (D)
-             & " is "        & Integer'Image (C));
+   Display_Result;
 end Tarea;
