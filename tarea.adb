a56d645188bbda264d67557bc0c5288a4cefc570
David Valenzuela <davidevc.32@gmail.com>
2020-05-23 19:38:10 -0400

    Renombrar funciones o procedimientos con nombres mas cortos y usarlos a ventaja

--- tarea.adb
+++ tarea.adb
@@ -3,26 +3,9 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Increment_By;
 
 procedure Tarea is
-    procedure Compute_A (V : Natural);
-    --  Forward declaration of Compute_A
+function Img (I : Integer) return String renames Integer'Image;
 
-    procedure Compute_B (V : Natural) is
-    begin
-      if V > 5 then
-         Put(V);
-          Compute_A (V - 1);
-         --  Call to Compute_A
-       end if;
-    end Compute_B;
-
-    procedure Compute_A (V : Natural) is
-    begin
-      if V > 2 then
-         Put(V);
-         Compute_B (V - 1);
-          --  Call to Compute_B
-       end if;
-    end Compute_A;
 begin
-   Compute_A (15);
+   Put_Line (Img (2));
+   Put_Line (Img (3));
 end Tarea;
