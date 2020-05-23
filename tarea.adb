29b4cc54c206210ab3a5a02e038aa2cd6bb1ea40
David Valenzuela <davidevc.32@gmail.com>
2020-05-23 19:57:46 -0400

    Funciones de incrementos llamada en el programa principal

--- tarea.adb
+++ tarea.adb
@@ -1,10 +1,34 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Week;
+with Operations;
 
 procedure Tarea is
-   use Week;
-   --  Make every entity of the Week package directly visible.
+   use Operations;
+
+   I : Integer := 0;
+   R : Integer;
+
+   procedure Display_Update_Values is
+      Incr : constant Integer := Get_Increment_Value;
+   begin
+      Put_Line (Integer'Image (I)
+                & " incremented by " & Integer'Image (Incr)
+                & " is "             & Integer'Image (R));
+      I := R;
+   end Display_Update_Values;
 begin
-   Put_Line ("First day of the week is " & Mon);
+   R := Increment_By (I);
+   Display_Update_Values;
+   R := Increment_By (I);
+   Display_Update_Values;
+
+   R := Increment_By (I, 5);
+   Display_Update_Values;
+   R := Increment_By (I);
+   Display_Update_Values;
+
+   R := Increment_By (I, 10);
+   Display_Update_Values;
+   R := Increment_By (I);
+   Display_Update_Values;
 end Tarea;
