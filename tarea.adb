0d43e60ea124147437766b63bebbcdf8ae1aa8c2
David Valenzuela <davidevc.32@gmail.com>
2020-05-23 19:47:30 -0400

    Otra forma de hacer uso de un paquete

--- tarea.adb
+++ tarea.adb
@@ -3,6 +3,8 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Week;
 
 procedure Tarea is
+   use Week;
+   --  Make every entity of the Week package directly visible.
 begin
-   Put_Line ("First day of the week is " & Week.Mon);
+   Put_Line ("First day of the week is " & Mon);
 end Tarea;
