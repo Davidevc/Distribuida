573c9c41aea588b3aae80b4105afba739d983d1b
David Valenzuela <davidevc.32@gmail.com>
2020-05-23 19:43:11 -0400

    Declaracion simple de un paquete y uso de este

--- tarea.adb
+++ tarea.adb
@@ -1,11 +1,8 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Increment_By;
+with Week;
 
 procedure Tarea is
-function Img (I : Integer) return String renames Integer'Image;
-
 begin
-   Put_Line (Img (2));
-   Put_Line (Img (3));
+   Put_Line ("First day of the week is " & Week.Mon);
 end Tarea;
