eee7dfba63ace63c333ef7b6e356d63258a97ec2
David Valenzuela <davidevc.32@gmail.com>
2020-05-26 17:44:39 -0400

    Tipado FUERTE

--- tarea.adb
+++ tarea.adb
@@ -1,36 +1,22 @@
 with Ada.Text_IO; use Ada.Text_IO;
-
+--STRONG TYPING
 procedure Tarea is
-   type T3  is digits 3;
-   type T15 is digits 15;
-   type T18 is digits 18;
-   D : Float := 2.5;
+    type Meters is new Float;
+   type Miles is new Float;
 
-   type T_Norm  is new Float range -1.0 .. 1.0;
-   E  : T_Norm;
-
-   procedure usoFlotante is
-      C1 : constant := 1.0e-4;
-      A : T3  := 1.0 + C1;
-      B : T18 := 1.0 + C1;
+   --  Function declaration, like procedure but returns a value.
+   function To_Miles (M : Meters) return Miles is
+   --                             ^ Return type
    begin
-   Put_Line ("The value of A is " & T3'Image (A));
-   Put_Line ("The value of B is " & T18'Image (B));
-   end usoFlotante;
+      return (Miles (M) * 1609.0) / 1000.0;
+      --         ^ Type conversion, from Meters to Miles
+   --  Now the code is correct
+   end To_Miles;
 
-
+   Dist_Imperial : Miles;
+   Dist_Metric   : constant Meters := 100.0;
 begin
-   Put_Line ("T3  requires " & Integer'Image (T3'Size) & " bits");
-   Put_Line ("T15 requires " & Integer'Image (T15'Size) & " bits");
-   Put_Line ("T18 requires " & Integer'Image (T18'Size) & " bits");
-
-   D := abs (D - 4.5);
-   Put_Line ("The value of D is " & Float'Image (D));
-   D := D ** 2 + 1.0;
-   Put_Line ("The value of D is " & Float'Image (D));
-
-   usoFlotante;
-
-   E := 1.0;
-   Put_Line ("The value of E is " & T_Norm'Image (E));
+   Dist_Imperial := To_Miles (Dist_Metric);
+   --ADA rechaza procedimientos en los cuales se mezclen variables. (FUERTEMENTE TIPADA)
+   Put_Line (Miles'Image (Dist_Imperial));
 end Tarea;
