57218551bfb4c42a2d7ed5f3ef483caa554d7025
David Valenzuela <davidevc.32@gmail.com>
2020-05-26 18:41:12 -0400

    ARRAY SIMPLE

--- tarea.adb
+++ tarea.adb
@@ -1,23 +1,20 @@
 with Ada.Text_IO; use Ada.Text_IO;
 --Component selection
 procedure Tarea is
+   type My_Int is range 0 .. 1000;
+   type Index is range 1 .. 5;
+   type My_Int_Array is array (Index) of My_Int;
+   --                                    ^ Type of elements
+   --                          ^ Bounds of the array
+   Arr : My_Int_Array := (2, 3, 5, 7, 11);
+   --                    ^ Array literal, called aggregate in Ada
 
-    type Month_Type is
-     (January, February, March, April, May, June, July,
-      August, September, October, November, December);
-
-   type Date is record
-      Day   : Integer range 1 .. 31;
-      Month : Month_Type;
-      Year  : Integer range 1 .. 3000 := 2032;
-   end record;
-
-   Some_Day : Date := (Day => 1, Month => January,Year => 2000);
 
 begin
-   Some_Day.Year := 2001;
-   Put_Line ("Day:" & Integer'Image (Some_Day.Day)
-             & ", Month: " & Month_Type'Image (Some_Day.Month)
-             & ", Year:" & Integer'Image (Some_Day.Year));
+   for I in Index loop
+      Put (My_Int'Image (Arr (I)));
+      --                     ^ Take the Ith element
+   end loop;
+   New_Line;
 
 end Tarea;
