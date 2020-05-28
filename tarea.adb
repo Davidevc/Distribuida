f20bed10a7ab84b08e4ebe82a3d37fa8b2a659c6
David Valenzuela <davidevc.32@gmail.com>
2020-05-28 11:55:21 -0400

    CLASE SEMAFOROS 28 DE MAYO

--- tarea.adb
+++ tarea.adb
@@ -1,20 +1,70 @@
-with Ada.Text_IO; use Ada.Text_IO;
---Component selection
-procedure Tarea is
-   type My_Int is range 0 .. 1000;
-   type Index is range 1 .. 5;
-   type My_Int_Array is array (Index) of My_Int;
-   --                                    ^ Type of elements
-   --                          ^ Bounds of the array
-   Arr : My_Int_Array := (2, 3, 5, 7, 11);
-   --                    ^ Array literal, called aggregate in Ada
+with Ada.Text_Io, Ada.Integer_Text_IO;
+use Ada.Text_Io, Ada.Integer_Text_IO;
+with TipoSemaforos;
 
+--EJEMPLO SEMAFORO
+procedure Tarea is
+package Paquete_Semaforos is new TipoSemaforos;
+  use Paquete_Semaforos;
+  miSemaforo : TSemaforo;
+
+  task Tarea1;
+  task Tarea2;
+
+  VariableComun: Integer := 0;
+
+  task body Tarea1 is
+       tmpT1 : Integer := 100;
+       tmp : Integer;
+  begin
+    for i in 1..10 loop
+	Wait(miSemaforo);
+	 Put("{");
+	 tmp := VariableComun;
+         tmp := tmp+tmpT1;
+	 delay(0.1);
+	 VariableComun := tmp;
+	 Put("}");
+	Signal(miSemaforo);
+
+	for k in 1 .. 5 loop
+		put_line("mi mama me mima");
+	end loop;
+
+    end loop;
+  end Tarea1;
+
+  task body Tarea2 is
+       tmpT2 : Integer := 1000;
+       tmp : Integer;
+  begin
+    for i in 1..10 loop
+	Wait(miSemaforo);
+	   Put("[");
+	   tmp := VariableComun;
+           tmp := tmp+tmpT2;
+	   delay(0.1);
+	   VariableComun := tmp;
+	   Put("]");
+	Signal(miSemaforo);
+
+	for k in 1 .. 5 loop
+		put_line("2+2=4");
+	end loop;
+
+    end loop;
+
+
+  end Tarea2;
 
 begin
-   for I in Index loop
-      Put (My_Int'Image (Arr (I)));
-      --                     ^ Take the Ith element
-   end loop;
-   New_Line;
-
+     for i in 1..10 loop
+	Wait(miSemaforo);
+           Put("<");
+           Put(VariableComun);
+           Put(">");
+	Signal(miSemaforo);
+     end loop;
+	Delay(3.0);
+        Put(VariableComun);
 end Tarea;
