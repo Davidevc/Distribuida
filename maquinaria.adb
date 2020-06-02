775025cd9e5b56804b948777a224f3211c121410
David Valenzuela <davidevc.32@gmail.com>
2020-06-02 19:16:08 -0400

    TAREA TERMINADA

--- maquinaria.adb
+++ maquinaria.adb
@@ -9,16 +9,16 @@ package Paquete_Semaforos is new TipoSemaforos;
       task Maquina_Sillin;
       task Maquina_Marco;
       task Maquina_Ruedas;
-      --task Maquina_Canastito;
-      --task Maquina_Frenos;
-      --task Maquina_Pedales;
+      task Maquina_Canastito;
+      task Maquina_Frenos;
+      task Maquina_Pedales;
       
       --************************* MAQUINA SILLIN *******************************
       task body Maquina_Sillin is
       begin
          Put_Line("MAQUINA SILLIN: Comienzo sillin");
          delay(4.0);
-         Put_Line("MAQUINA SILLIN: Sillin Listo");
+         Put_Line("MAQUINA SILLIN: Sillin listo");
       end Maquina_Sillin;
       --************************* MAQUINA SILLIN *******************************
       
@@ -34,7 +34,7 @@ package Paquete_Semaforos is new TipoSemaforos;
          begin
             Put_Line("MAQUINA FUNDIDORA: Comienzo a fundir");
             delay(7.0);
-            Put_Line("MAQUINA FUNDIDORA: Fundido Listo");
+            Put_Line("MAQUINA FUNDIDORA: Fundido listo");
             Signal(SemaforoFunde);
          end Funde;
    
@@ -45,7 +45,7 @@ package Paquete_Semaforos is new TipoSemaforos;
             Wait(SemaforoFunde);
             Put_Line("MAQUINA PINTADO: Puedo pintar y pinto");
             delay(4.0);
-            Put_Line("MAQUINA PINTADO: Pintado Listo");
+            Put_Line("MAQUINA PINTADO: Pintado listo");
             Signal(MarcoListo);
          end Pinta;
       
@@ -84,6 +84,34 @@ package Paquete_Semaforos is new TipoSemaforos;
          end loop;
       end Maquina_Ruedas;
       --************************* MAQUINA RUEDA *******************************
+      
+      --************************* MAQUINA CANASTITO ****************************
+      task body Maquina_Canastito is         
+      begin
+         Put_Line("MAQUINA CANASTITO: Comienzo el canastito");
+         delay(7.0);
+         Put_Line("MAQUINA CANASTITO: Canastito listo");
+      end Maquina_Canastito;
+      --************************* MAQUINA CANASTITO ****************************
+      
+      --************************* MAQUINA FRENOS *******************************
+      task body Maquina_Frenos is
+      begin
+         Put_Line("MAQUINA FRENOS: Comienzo los frenos");
+         delay(3.0);
+         Put_Line("MAQUINA FRENOS: Frenos listos");
+      end Maquina_Frenos;
+      --************************* MAQUINA FRENOS *******************************
+
+      --************************* MAQUINA PEDALES ******************************
+      task body Maquina_Pedales is 
+      begin
+         Put_Line("MAQUINA PEDALES: Comienzo los pedales");
+         delay(10.0);
+         Put_Line("MAQUINA PEDALES: Pedales listos");
+      end Maquina_Pedales;
+      --************************* MAQUINA PEDALES ******************************
+      
    begin
       null;  
    end armarBicicleta;
--- tarea.adb
+++ tarea.adb
@@ -8,18 +8,51 @@ procedure Tarea is
 package Paquete_Semaforos is new TipoSemaforos;
    use Paquete_Semaforos;
    use Maquinaria;
+
    Semaforo1 : TSemaforo;
-   task Armador_Bicicleta;
+   Semaforo2 : TSemaforo;
 
-   task body Armador_Bicicleta is
+   task Operador_Armador_Bicicleta;
+   task Operador_Limpia;
+   task Operador_embalaje;
 
+   task body Operador_Armador_Bicicleta is
    begin
-      Put_Line("OPERARIO A: Enciendo las maquinas para la bicicleta");
+      Wait(Semaforo2);
+      Put_Line("OPERARIO ARMADO: Enciendo las maquinas para la bicicleta");
+      delay(0.4);
       armarBicicleta;
-      Put_Line("OPERARIO A: Tengo Todas las piezas, la arme");
-  end Armador_Bicicleta;
+      Put_Line("OPERARIO ARMADO: Tengo Todas las piezas, la arme");
+      Put_Line("OPERARIO ARMADO: Bicicleta armada se la delego a el limpiador");
+      Signal(Semaforo1);
+   end Operador_Armador_Bicicleta;
+
+   task body Operador_Limpia is
+   begin
+      delay(0.2);
+      Put_Line("OPERARIO LIMPIADOR: Espero la bicicleta terminada para empezas a limpiar");
+      Wait(Semaforo1);
+      Put_Line("OPERARIO LIMPIADOR: Tengo la bicicleta en mis manos y la estoy limpiando");
+      delay(5.0);
+      Put_Line("OPERARIO LIMPIADOR: Termine de limpiarla y se la delego al operario de embalaje");
+      Signal(Semaforo1);
+   end Operador_Limpia;
+
+   task body Operador_embalaje is
+   begin
+      delay(0.3);
+      Put_Line("OPERARIO EMBALAJE: Espero la bicicleta limpiada para empezas a embalar");
+      Wait(Semaforo1);
+      Put_Line("OPERARIO EMBALAJE: Tengo la bicicleta en mis manos y la estoy embalando");
+      delay(2.0);
+      Put_Line("OPERARIO EMBALAJE: Termine el embalaje");
+      Signal(Semaforo2);
+  end Operador_embalaje;
 
 
 begin
-   null;
+   Wait(Semaforo1);
+   delay(0.2);
+   Wait(Semaforo2);
+   Put_Line("*************************** BICICLETA LISTA ***************************");
 end Tarea;
